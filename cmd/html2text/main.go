7eb09e562d5e485c9d83ec280819a043f6c71a38
lsy88 <1845023541@qq.com>
2024-08-30 15:07:06 +0800

    feat:html2text

---
+++ cmd/html2text/main.go
@@ -0,0 +1,20 @@
+package main
+
+//import (
+//	"bufio"
+//	"fmt"
+//	"os"
+//
+//	"github.com/lsy88/html2text"
+//)
+//
+//func main() {
+//	reader := bufio.NewReader(os.Stdin)
+//	opts := html2text.Options{}
+//	out, err := html2text.FromReader(reader, opts)
+//	if err != nil {
+//		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+//		os.Exit(1)
+//	}
+//	fmt.Println(out)
+//}
